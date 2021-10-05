6ea23d6fe0bbb703f8bd0d6ae78f41e7f287d75d
Ciumax <ciumek2991@gmail.com>
2021-10-05 19:14:52 +0200

    .

--- AreTheyTheSame/Program.cs
+++ AreTheyTheSame/Program.cs
@@ -1,23 +1,26 @@
 ﻿using System;
+
 class AreTheySame
 {
-  public static bool comp(int[] a, int[] b)
-  {
-    if(a == null || b == null || a.Length != b.Length)
-      return false;
-    Array.Sort(a);
-    Array.Sort(b);
-    int count = 0;
-    for( int i = 0; i < a.Length; i++)
+    public static bool comp(int[] a, int[] b)
     {
-      if(a[i] != Math.Sqrt(b[i]))
-      {
-        count++;
-      }
+        if (a == null || b == null || a.Length != b.Length)
+        {
+            return false;
+        }
+        Array.Sort (a);
+        Array.Sort (b);
+        int count = 0;
+        for (int i = 0; i < a.Length; i++)
+        {
+            if (a[i] != Math.Sqrt(b[i]))
+            {
+                count++;
+            }
+        }
+        if (count != 0)
+            return false;
+        else
+            return true;
     }
-    if(count != 0 )
-      return false;
-    else
-      return true;
-  }
 }
--- DoesMyNumber/Program.cs
+++ DoesMyNumber/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+
 namespace DoesMyNumber
 {
     public class Kata
@@ -9,29 +10,27 @@ namespace DoesMyNumber
             Narcissistic(153);
             Narcissistic(1652);
         }
-  public static bool Narcissistic(int value)
-  {
-     List<int> digits = new List<int>();
-     int ans = 0, save = value;
-    for (; value != 0; value /= 10)
-     {
-             digits.Add(value % 10);
-     }
-    for( int i = 0; i < digits.Count; i++)
-    {
-        ans += FastPow(digits[i],digits.Count);
-    }
-    Console.WriteLine(ans + "  " + save);
-    if( save == ans){
-    return true;
-    }else 
-    return false;
-  }
 
- public static int FastPow(int x, int pow)
-    {
-        Console.WriteLine(x + "  " + pow);
-        return (int)Math.Pow(x, pow);
-    }
+        public static bool Narcissistic(int value)
+        {
+            List<int> digits = new List<int>();
+            int
+                ans = 0,
+                save = value;
+            for (; value != 0; value /= 10)
+            {
+                digits.Add(value % 10);
+            }
+            for (int i = 0; i < digits.Count; i++)
+            {
+                ans += PowerOF(digits[i], digits.Count);
+            }
+            return save == ans;
+        }
+
+        public static int PowerOF(int x, int pow)
+        {
+            return (int) Math.Pow(x, pow);
+        }
     }
 }
