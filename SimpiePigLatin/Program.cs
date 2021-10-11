e5f5c7b01ed0ca29a10b97ed22a46458a598a03b
Ciumax <ciumek2991@gmail.com>
2021-10-11 14:39:58 +0200

    Solutions and Updates

---
+++ SimpiePigLatin/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace SimpiePigLatin
+{
+    public class Kata
+    {
+        static void Main(string[] args)
+        {
+            PigIt("Pig latin is cool");
+        }
+
+        public static string PigIt(string str)
+        {
+            string ay = "ay";
+            string[] Arr = str.Split(" ");
+            string result = "";
+            Regex rgx = new Regex("[^A-Za-z0-9]");
+            foreach (string element in Arr)
+            {
+                if (!rgx.IsMatch(element))
+                {
+                    result +=
+                        element.Substring(1, element.Length - 1) +
+                        element[0] +
+                        ay +
+                        " ";
+                }
+                else
+                {
+                    result += element;
+                }
+            }
+            return result.Trim();
+        }
+    }
+}
