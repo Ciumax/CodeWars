f9e13ae8853427b6d2b32fdb1da8c0043b386245
Ciumax <ciumek2991@gmail.com>
2021-10-15 17:31:50 +0200

    Solution

---
+++ RGBtoHex/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+
+namespace RGBtoHex
+{
+    public class Kata
+    {
+        static void Main(string[] args)
+        {
+            Kata.Rgb(148, 0, 211);
+        }
+        public static string Rgb(int r, int g, int b)
+        {
+            if(r > 255) r = 255;
+            if(g > 255) g = 255;
+            if(b > 255) b = 255;
+            if(r < 0) r = 0;
+            if(g < 0) g = 0;
+            if(b < 0) b = 0;
+            string hexValue = r.ToString("X2") + g.ToString("X2") + b.ToString("X2");
+            return hexValue;
+        }
+    }
+}
---
+++ RGBtoHex/Readme.md
@@ -0,0 +1,1 @@
+[Link to Kata](https://www.codewars.com/kata/513e08acc600c94f01000001)
