a81b2ac2ad57bddde66218fe7784cbed1e3d5808
Ciumax <ciumek2991@gmail.com>
2021-11-24 14:19:11 +0100

    NotVery Solution

---
+++ NotVerSecure/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NotVerSecure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public static bool Alphanumeric(string str)
+        {
+            if (String.IsNullOrEmpty(str)) return false;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (Char.IsLetterOrDigit(str[i]) == false) return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ NotVerSecure/Readme.md
@@ -0,0 +1,1 @@
+[Link to kata](https://www.codewars.com/kata/526dbd6c8c0eb53254000110)
